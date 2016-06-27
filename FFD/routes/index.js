40f2295b820e4bb3c1a65e4b747078e4c4eddedc
lqllyf <78543900@qq.com>
2016-06-27 09:17:20 +0800

    add

--- FFD/routes/index.js
+++ FFD/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('liveForMatches', { title: 'Expresses' });
+  res.render('liveForMatches2', { title: 'Expresses' });
 });
 
 module.exports = router;
